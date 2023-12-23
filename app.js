3877d93155afd41c590b7361a1c87465edbc3c7b
Ganesh Yenurkar <ganeshyenurkar5@gmail.com>
2023-12-23 13:40:14 +0530

    fix app.js bug

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Node js')
+// console.log('Node js')
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3200;
+const dotenv = require("dotenv").config();
+
+// MIDDLEWARES
+app.use(express.json());
+// app.use(dotenv.config());
+
+app.get("/", (req, res) => {
+  res.send("NODE JS PRACTICE PROJECT");
+});
+
+app.listen(PORT, () => [
+  console.log(`Server is running on port http://localhost:${PORT}`),
+]);
